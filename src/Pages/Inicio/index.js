da0c5ecf22aa52f573217673443c8d9827bc3622
Tarcisio <caninosbrancos10031@gmail.com>
2022-10-19 22:09:45 -0300

    commit

--- src/Pages/Inicio/index.js
+++ src/Pages/Inicio/index.js
@@ -1,4 +1,9 @@
 import { Main, Main1, Main2, FundoImg } from "./styled";
+import precos30 from "../../assets/img/precos30.png";
+import Transparent from "../../assets/img/Transparent.png";
+
+
+
 export default function Inicio() {
   return (
     <div className="inicio">
@@ -8,32 +13,29 @@ export default function Inicio() {
 
         <Main>
           <>
-            <img src="./imgs/Transparent.png" alt="" />
-            <img src="./imgs/precos30.png" alt="" />
+            <img src={Transparent} alt="erroTiara" />
+            <img src={precos30} alt="erropreco" />
           </>
-          <h1>ID: 1</h1>
+
+          <h6>ID: 1</h6>
         </Main>
+
         <Main1>
           <>
-            <img src="./imgs/Transparent.png" alt="" />
-            <img src="./imgs/precos30.png" alt="" />
+            <img src={Transparent} alt="erroTiara" />
+            <img src={precos30} alt="erropreco" />
           </>
-          <h1>ID: 2</h1>
+          <h6>ID: 2</h6>
         </Main1>
 
         <Main2>
           <>
-            <img src="./imgs/Transparent.png" alt="" />
-            <img src="./imgs/precos30.png" alt="" />
+            <img src={Transparent} alt="erroTiara" />
+            <img src={precos30} alt="erropreco" />
           </>
-          <h1>ID: 3</h1>
+          <h6>ID: 3</h6>
         </Main2>
-
-
-
       </FundoImg>
-
-
 
     </div >
   );
--- src/Pages/Inicio/styled.js
+++ src/Pages/Inicio/styled.js
@@ -1,21 +1,25 @@
 import styled from "styled-components";
 
+import milpxfundo1 from "../../assets/img/1000pxfundo1.jpg";
+import milpxfundo2 from "../../assets/img/500pxfundo1.jpg";
+import milpxfundo3 from "../../assets/img/1fundopequeno.jpg";
+
 export const Main = styled.article`
 background: #f388e08c;
 
 border-radius: 50px 0px 0px 0px ;
 color: black;
 width: 30%;
-height: 29%;
+height: 28%;
 
 position: absolute;
-bottom: 56.9%;
+bottom: 57%;
 left: 3%;
 text-align: center;
 
 @media screen and (max-width: 800px){
 left: 5%;
-bottom: 0;
+bottom:  0%;
 color:white;
   
 }
@@ -64,31 +68,28 @@ text-align: center;
 
 
 export const FundoImg = styled.article`
-position: absolute;
-
-background-image: url("/imgs/1000pxfundo1.jpg");
-background-position: center right;
+background-color: aliceblue;
+background-image: url(${milpxfundo1});
 background-size: cover;
+background-position: center right;
+
 bottom: -1;
 color: yellow;
 width: 100vw;
 height: 85vh;
+position: absolute;
 
 @media screen and (max-width: 800px){
-  background-image: url("/imgs/500pxfundo1.jpg");
-
-  
+  background-image: url(${milpxfundo2});
 }
+
 
 @media screen and (max-width: 590px){
-  background-image: url("/imgs/1fundopequeno.jpg");
+  background-image: url(${milpxfundo3});
   background-position: center center;
   height: 65vh;
-   
-
-
-  
 }
+
 
 
 `;
